6f5e297c391c1363f93d9d7ececc5973a82e1b35
Paulo Junior <jr@live.at>
2021-08-23 17:47:41 -0300

    Setting up the user interface part 3

---
+++ uber/ViewController/CadastroViewContoller.swift
@@ -0,0 +1,24 @@
+//
+//  CadastroViewContoller.swift
+//  uber
+//
+//  Created by Junior Ferreira on 23/08/21.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+}
